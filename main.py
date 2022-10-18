c89d6b88b56885e8b602b2b6d4d081fdc42d85af
gmalachias <gmalaxias@gmail.com>
2022-10-18 22:32:26 +0300

    Updated with using json, and implemented a search button to show the user the password.

--- main.py
+++ main.py
@@ -2,24 +2,44 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password_letters = [choice(letters) for _ in range(randint(8, 10))]
-password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password_list = password_letters + password_symbols + password_numbers
-shuffle(password_list)
-password = "".join(password_list)
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+# ---------------------------- SEARCH ------------------------------- #
+
+
+def find_password():
+    try:
+        with open("data_file.json", "r") as data_file:
+            website = website_entry.get()
+            data = json.load(data_file)
+            if website in data:
+                messagebox.showinfo(title="Info", message=f"Website: {website}, Password: {data[website]['password']}")
+            else:
+                messagebox.showerror(title="Error", message="Website not found.")
+    except FileNotFoundError:
+        messagebox.showerror(title="File Error", message="No Data File found.")
+    except KeyError:
+        messagebox.showerror(title="Website Error", message="No details for the website exists")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def generate():
-    password_entry.insert(0,password)
-    pyperclip.copy(password)
 
 
 def delete():
@@ -31,17 +51,31 @@ def add():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password,
+        }
+    }
+
     if website == "" or password == "":
         messagebox.showerror(title="Error", message="Empty slots")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\nIs it ok to save?")
         if is_ok:
-            data_file = open("data_file.txt", "a")
-            data_file.write(f"{website} | {email} | {password}\n")
-            data_file.close()
+            try:
+                with open("data_file.json", "r") as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data_file.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data_file.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
             delete()
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.config(padx=50, pady=50)
@@ -53,12 +87,15 @@ image = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=image)
 canvas.grid(column=1, row=0,)
 
-website_label = Label(text="Website:" )
+website_label = Label(text="Website:")
 website_label.grid(column=0, row=1,)
 
-website_entry = Entry(width=48)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=34)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
+
+search_button = Button(text="Search", width=10, command=find_password)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
